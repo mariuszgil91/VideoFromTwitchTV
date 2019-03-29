59ab9cf4e1fb545cac38ee4e54b6b29de2f048cb
Mariusz Gil <mariusz.gil91@gmail.com>
2019-03-29 21:46:47 +0100

    add table view required methods

--- VideoFromTwitchTV/Controller/ViewController.swift
+++ VideoFromTwitchTV/Controller/ViewController.swift
@@ -8,13 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    // Data model: These strings will be the data for the table view cells
+    let gameListArray: [String] = ["DOTA", "Fortnite", "League of Legend", "Sekiro", "Bloodborne"]
+    
+    // cell reuse id (cells that scroll out of view can be reused)
+    let cellReuseIdentifier = "cell"
+    
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    @IBOutlet weak var gamesListTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.gamesListTableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+        
+        gamesListTableView.delegate = self
+        gamesListTableView.dataSource = self
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return gameListArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // create a new cell if needed or reuse an old one
+        let cell : UITableViewCell = (self.gamesListTableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as UITableViewCell?)!
+        
+        // set the text from the data model
+        cell.textLabel?.text = self.gameListArray[indexPath.row]
+        
+        return cell
+    }
+    
+    // method to run when table view cell is tapped
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You tapped cell number \(indexPath.row).")
+        //performSegue(withIdentifier: "goToVideoView", sender: self)
+    }
 
 }
 
