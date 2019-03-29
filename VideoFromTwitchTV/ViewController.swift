73d3fb4eb4ea17980e2118b40d7f8b1bfb10738d
Mariusz Gil <mariusz.gil91@gmail.com>
2019-03-29 21:06:51 +0100

    Initial Commit

---
+++ VideoFromTwitchTV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoFromTwitchTV
+//
+//  Created by Mariusz Gil on 29/03/2019.
+//  Copyright © 2019 Mariusz Gil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
